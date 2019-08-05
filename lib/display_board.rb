64d82c58e1cee87053adec6812b058d152670f94
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2019-08-05 18:55:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,14 @@ board = ["O", "X", "X", "X", "X", " ", "X", "O", " "]
 
 def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  
+  expect(rows[2]).to eq("   |   |   ")
+  expect(rows[3]).to eq("-----------")
+  expect(rows[4]).to eq("   |   |   ")
 end
 
 display_board(board)
